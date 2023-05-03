2b9d38c0a68e058f27503643e9ff95028ce3672a
Ankit <2020csb1072@iitrpr.ac.in>
2023-05-03 23:38:37 +0530

    added fuzzy logic and minor design changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,18 @@
 import React, { useEffect } from "react";
 import AuthForm from "../common/AuthForm";
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../Providers/userProvider";
 export default function Login() {
   const navigate = useNavigate();
-  const {handleLogin } = useContext(UserContext);
+  const { handleLogin } = useContext(UserContext);
   const onSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted");
-    const userData = {"name":"Neil Sims","email":"neil_sims@ymail.com"}
+    const userData = { name: "Neil Sims", email: "neil_sims@ymail.com" };
     handleLogin(userData);
-    // makes a call to the backend to login
-    // if successful, redirect to dashboard
 
-    navigate('/dashboard'); 
-
+    navigate("/dashboard");
   };
   const props = {
     title: "Flowbite",
@@ -24,7 +21,7 @@ export default function Login() {
 
     secondaryLink: "/forgot-password",
     secondaryText: "Forgot password?",
-    
+
     alternativeTextPre: "Or",
     alternativeLink: "/register",
     alternativeText: "Don't have an account?",
